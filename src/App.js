de94e05076e3340f92646861712b0d5afc8579b8
anon2020s <66768817+anon2020s@users.noreply.github.com>
2023-10-14 09:18:22 +0000

    fix annotation

--- src/App.js
+++ src/App.js
@@ -11,46 +11,41 @@ import QRCode from "react-qr-code";
 const ec = new EC("secp256k1");
 const g = ec.g;
 
-function generateStealthErgoTree(
-    grHex: String,
-    gyHex: String,
-    urHex: String,
-    uyHex: String
-): ErgoTree {
+function generateStealthPaymentErgoTree(grHex, gyHex, urHex, uyHex) {
     return new ErgoTree(
         `10040e21${grHex}0e21${gyHex}0e21${urHex}0e21${uyHex}ceee7300ee7301ee7302ee7303`
     );
 }
 
-// validate stealthPK should be in format: "stealth" + Base58 encoded of (pk + leftmost 4 bytes blake2b256(pk))
-function validateStealthPK(stealthPK: String): void {
-    const splitPK = stealthPK.split("stealth");
-    if (splitPK.length !== 2) throw "invalid stealthPk";
-    const decodedPK = Base58.decode(splitPK[1]);
-    const mainPK = decodedPK.slice(0, decodedPK.length - 4);
-    const checksumPK = decodedPK.slice(-4);
-    const leftmost4BytesHashPK = blake2b(mainPK, null, 32).slice(0, 4)
+// validate stealthAddress should be in format: "stealth" + Base58 encoded of (pk + leftmost 4 bytes blake2b256(pk))
+function validateStealthAddress(stealthAddress) {
+    const splitAddress = stealthAddress.split("stealth");
+    if (splitAddress.length !== 2) throw Error("invalid stealthAddress");
+    const decodedAddress = Base58.decode(splitAddress[1]);
+    const mainAddress = decodedAddress.slice(0, decodedAddress.length - 4);
+    const checksumAddress = decodedAddress.slice(-4);
+    const leftmost4BytesHashAddress = blake2b(mainAddress, null, 32).slice(0, 4)
     // validate checksum
-    if (!isEqual(checksumPK, leftmost4BytesHashPK)) throw "invalid checksum";
+    if (!isEqual(checksumAddress, leftmost4BytesHashAddress)) throw Error("invalid checksum");
 }
 
 function App() {
-    const [stealthPK, setStealthPK] = useState("");
+    const [stealthAddress, setStealthAddress] = useState("");
     const [result, setResult] = useState("");
 
     function handleChange(event) {
-        setStealthPK(event.target.value)
+        setStealthAddress(event.target.value)
         !event.target.value && setResult("")
     }
 
-    function generateAddress() {
+    function generatePaymentAddress() {
         try {
-            validateStealthPK(stealthPK);
-            const splitPK = stealthPK.split("stealth");
-            const decodedPK = Base58.decode(splitPK[1]);
-            // remove checksum and get main PK
-            const mainPK = decodedPK.slice(0, decodedPK.length - 4);
-            const u = ec.curve.decodePoint(mainPK);
+            validateStealthAddress(stealthAddress);
+            const splitAddress = stealthAddress.split("stealth");
+            const decodedAddress = Base58.decode(splitAddress[1]);
+            // remove checksum and get main Address
+            const mainAddress = decodedAddress.slice(0, decodedAddress.length - 4);
+            const u = ec.curve.decodePoint(mainAddress);
             const r = new BN(rand(32));
             const y = new BN(rand(32));
 
@@ -59,52 +54,52 @@ function App() {
             const ur = u.mul(r).encode("hex", true);
             const uy = u.mul(y).encode("hex", true);
 
-            const generatedAddress = generateStealthErgoTree(
+            const generatedPaymentAddress = generateStealthPaymentErgoTree(
                 gr,
                 gy,
                 ur,
                 uy
             ).toAddress(Network.Mainnet);
-            setResult(`${generatedAddress.toString(Network.Mainnet)}`);
+            setResult(`${generatedPaymentAddress.toString(Network.Mainnet)}`);
         } catch (error) {
-            setResult(`Error: ${error}`);
+            setResult(`${error}`);
         }
     }
 
     return (
         <div className="container">
             <div className="header">
-                <img className="imageStealthPK" src={`${process.env.PUBLIC_URL}/img.png`} id="image-logo"/>
+                <img className="imageStealthAddress" src={`${process.env.PUBLIC_URL}/img.png`} id="image-logo" alt={""}/>
                 <div className="header-texts">
-                    <h1 className="headerStealth">Stealth Address Generator</h1>
-                    <p className="stealthPKText">
-                        <b>What is a stealth PK?</b>
-                        <text> A receiver can generate a Stealth Public Key to preserve his privacy.
+                    <h1 className="headerStealth">Stealth Payment Address Generator</h1>
+                    <p className="stealthAddressText">
+                        <b>What is a stealth Address?</b>
+                        <text> A receiver can generate a Stealth Address to preserve his privacy.
                             It starts with "stealth" word and allows any sender to create new payment addresses for this
                             receiver without any interaction.
                         </text>
                         <br/>
 
-                        <b>How to generate a stealth PK?</b>
+                        <b>How to generate a stealth Address?</b>
                         <text> It can be generated in the ErgoMixer.</text>
                         <br/>
 
-                        <b>Can I send funds to a stealth PK?</b>
+                        <b>Can I send funds to a stealth Address?</b>
                         <text> No, use this address generator to create a new payment address and use it as the
                             receiver's address in any wallet.
                         </text>
                     </p>
                 </div>
             </div>
-            <label htmlFor="stealthPK" className="margin">Receiver's Stealth PK:</label>
+            <label htmlFor="stealthAddress" className="margin">Receiver's Stealth Address:</label>
             <textarea
                 placeholder="Enter Input"
                 className="margin"
-                id="stealthPK"
-                value={stealthPK}
+                id="stealthAddress"
+                value={stealthAddress}
                 onChange={handleChange}
             />
-            <button className="margin" onClick={generateAddress}>Generate New Address</button>
+            <button className="margin" onClick={generatePaymentAddress}>Generate New Payment Address</button>
             {
                 result &&
                 <div className="result-container margin">
